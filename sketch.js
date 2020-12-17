dfddaf3f254006ed1e1ebc8a96df7ca437489026
ROSHANSINGH27 <75154879+ROSHANSINGH27@users.noreply.github.com>
2020-12-17 09:07:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+var bullet,wall,thickness
+var speed,weight,bullet
+
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed = random(30,52)
+  weight=random(233,321)
+  thickness=random(22,83)
+
+  bullet=createSprite(50, 200, 50, 10);
+  bullet.velocityX=speed
+  bullet.shapeColor=color(255)
+
+
+  wall=createSprite(1200,200,thickness,height/2)
+  wall.shapeColor=color(80,80,80)
+}
+
+
+function draw() {
+  background(0); 
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0
+    var deformation=0.5*weight*speed*speed/(thickness*thickness*thickness)
+    if(damage>10){
+      wall.shapeColor=color(255,0,0)
+    }
+    if(damage<10){
+      wall.shapeColor=color(0,255,0)
+    }
+    }
+ 
+  drawSprites();
+} 
